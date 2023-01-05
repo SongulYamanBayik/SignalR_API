f280954cd04345942cb68f8857230c498f2c3639
Songül YAMAN BAYIK <snglymn@gmail.com>
2023-01-05 17:55:19 +0300

    Add project files.

---
+++ SignalR_API/Hubs/MyHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalR_API.Hubs
+{
+    public class MyHub: Hub
+    {
+        public static List<string> Names { get; set; }
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+    }
+}
